de6fe91a9c2ad4d3d562e2650c859c47def23604
CSR-24 <chetan.raikwal@yahoo.com>
2021-09-11 13:43:23 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 // Import stylesheets
 import './style.css';
 
+const inputObj = {
+  sample: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  rotateBy: 1
+};
+
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const appContainer = document.getElementById('container');
+
+const clear = function() {
+  appContainer.innerHTML = '';
+};
+
+const renderContainer = function(input, container) {
+  clear();
+  let element = '';
+  input.forEach(value => {
+    element = document.createElement('div');
+    element.classList = 'box';
+    element.innerHTML = value;
+    container.appendChild(element);
+  });
+};
+
+/**
+ * In place rotation.
+ * @input is an array and holds the reference of the array
+ * being rotated.
+ */
+const rotateArray = function(input) {
+  if (input.rotateBy > input.sample.length) {
+    input.rotateBy = input.rotateBy % input.sample.length;
+  }
+  const slice1 = input.sample.slice(input.rotateBy);
+  const slice2 = input.sample.slice(0, input.rotateBy);
+  input.sample = slice1.concat(slice2);
+};
+
+renderContainer(inputObj.sample, appContainer);
+rotateArray(inputObj);
+renderContainer(inputObj.sample, appContainer);
