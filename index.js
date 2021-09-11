41736faaa4cb31af3f3181188aeacd5619f5daae
CSR-24 <chetan.raikwal@yahoo.com>
2021-09-11 23:06:32 +0530

    3 files modified

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ import './style.css';
 
 const inputObj = {
   sample: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  rotateBy: 1
+  rotateBy: 0
 };
 
 const appContainer = document.getElementById('container');
+const rotateByNumber = document.getElementById('rotateByNumber');
+const rotateBy = document.getElementById('rotateBy');
+const reset = document.getElementById('reset');
+
+const rotate = function(inputObj) {
+  rotateArray(inputObj);
+  renderContainer(inputObj.sample, appContainer);
+};
+
+rotateBy.addEventListener('click', () => {
+  inputObj.rotateBy = rotateByNumber.value;
+  rotate(inputObj);
+});
+
+reset.addEventListener('click', () => {
+  inputObj.sample = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  inputObj.rotateBy = 0;
+  renderContainer(inputObj.sample, appContainer);
+});
 
 const clear = function() {
   appContainer.innerHTML = '';
@@ -32,11 +51,11 @@ const rotateArray = function(input) {
   if (input.rotateBy > input.sample.length) {
     input.rotateBy = input.rotateBy % input.sample.length;
   }
+
   const slice1 = input.sample.slice(input.rotateBy);
   const slice2 = input.sample.slice(0, input.rotateBy);
   input.sample = slice1.concat(slice2);
 };
 
-renderContainer(inputObj.sample, appContainer);
-rotateArray(inputObj);
+rotateByNumber.value = inputObj.rotateBy;
 renderContainer(inputObj.sample, appContainer);
